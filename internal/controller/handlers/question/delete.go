c094fdf73102dbc4af3331c254cc557fad020bcf
Edgar Rupshis <73851167+ERupshis@users.noreply.github.com>
2023-11-07 00:28:05 +0200

    controller. handlers. question. delete. added impl and test

---
+++ internal/controller/handlers/question/delete.go
@@ -0,0 +1,31 @@
+package question
+
+import (
+	"fmt"
+
+	"github.com/erupshis/revtracker/internal/constants"
+	"github.com/erupshis/revtracker/internal/controller/handlers/utils"
+	"github.com/erupshis/revtracker/internal/logger"
+	"github.com/erupshis/revtracker/internal/storage"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Delete(storage storage.BaseStorage, log logger.BaseLogger) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		ID, err := utils.GetIDFromParams(c)
+		if err != nil {
+			log.Info("%s get ID from params: %v", fmt.Sprintf(packagePath, constants.Select), err)
+			c.Status(fiber.StatusBadRequest)
+			return nil
+		}
+
+		if err = storage.DeleteQuestionByID(c.Context(), ID); err != nil {
+			log.Info("%s failed to delete: %v", fmt.Sprintf(packagePath, constants.Delete), err)
+			c.Status(fiber.StatusInternalServerError)
+			return nil
+		}
+
+		c.Status(fiber.StatusOK)
+		return nil
+	}
+}
