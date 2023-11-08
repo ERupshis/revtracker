4862f9f16a84b5e724aea7f1d26033d35cb7653a
Edgar Rupshis <73851167+ERupshis@users.noreply.github.com>
2023-11-08 13:30:22 +0200

    controller. handlers. data. update. added impl and test

--- internal/controller/handlers/data/update.go
+++ internal/controller/handlers/data/update.go
@@ -1,6 +1,13 @@
 package data
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/erupshis/revtracker/internal/constants"
+	"github.com/erupshis/revtracker/internal/controller/handlers/utils"
+	"github.com/erupshis/revtracker/internal/data"
+	utilsData "github.com/erupshis/revtracker/internal/data/utils"
 	"github.com/erupshis/revtracker/internal/logger"
 	"github.com/erupshis/revtracker/internal/storage"
 	"github.com/gofiber/fiber/v2"
@@ -8,6 +15,56 @@ import (
 
 func Update(storage storage.BaseStorage, log logger.BaseLogger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		homeworkData := &data.Data{}
+
+		if err := json.Unmarshal(c.Body(), homeworkData); err != nil {
+			log.Info("%s failed to parse request body: %v", fmt.Sprintf(packagePath, constants.Update), err)
+			c.Status(fiber.StatusBadRequest)
+			return nil
+		}
+
+		if homeworkData.Homework.ID <= 0 {
+			ID, err := utils.GetIDFromParams(c)
+			if err != nil {
+				log.Info("%s get ID from params: %v", fmt.Sprintf(packagePath, constants.Update), err)
+				c.Status(fiber.StatusBadRequest)
+				return nil
+			}
+
+			homeworkData.Homework.ID = ID
+		}
+
+		if err := utilsData.ValidateHomeworkData(&homeworkData.Homework); err != nil {
+			log.Info("%s incorrect homework data: %v", fmt.Sprintf(packagePath, constants.Update), err)
+			c.Status(fiber.StatusBadRequest)
+			return nil
+		}
+
+		if err := utilsData.ValidateQuestionsData(homeworkData.Homework.Questions); err != nil {
+			log.Info("%s incorrect questions data: %v", fmt.Sprintf(packagePath, constants.Update), err)
+			c.Status(fiber.StatusBadRequest)
+			return nil
+		}
+
+		if err := storage.UpdateData(c.Context(), homeworkData); err != nil {
+			log.Info("%s failed to add: %v", fmt.Sprintf(packagePath, constants.Update), err)
+			c.Status(fiber.StatusInternalServerError)
+			return nil
+		}
+
+		response, err := json.Marshal(homeworkData)
+		if err != nil {
+			log.Info("%s failed to marshal json for response body", fmt.Sprintf(packagePath, constants.Update))
+			c.Status(fiber.StatusInternalServerError)
+			return nil
+		}
+
+		if _, err = c.Write(response); err != nil {
+			log.Info("%s failed to write response: %v", fmt.Sprintf(packagePath, constants.Update), err)
+			c.Status(fiber.StatusInternalServerError)
+			return nil
+		}
+
 		c.Set("Content-Type", "application/json")
 		c.Status(fiber.StatusOK)
 		return nil
---
+++ internal/controller/handlers/data/update_test.go
@@ -0,0 +1,358 @@
+package data
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/erupshis/revtracker/internal/logger"
+	"github.com/erupshis/revtracker/internal/storage"
+	utilsReform "github.com/erupshis/revtracker/internal/storage/manager/reform/utils"
+	"github.com/erupshis/revtracker/internal/utils"
+	"github.com/erupshis/revtracker/mocks"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpdate(t *testing.T) {
+	testLog, _ := logger.CreateZapLogger("info")
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockStorage := mocks.NewMockBaseStorage(ctrl)
+	gomock.InOrder(
+		mockStorage.EXPECT().UpdateData(gomock.Any(), gomock.Any()).Return(nil),
+		mockStorage.EXPECT().UpdateData(gomock.Any(), gomock.Any()).Return(nil),
+		mockStorage.EXPECT().UpdateData(gomock.Any(), gomock.Any()).Return(nil),
+		mockStorage.EXPECT().UpdateData(gomock.Any(), gomock.Any()).Return(fmt.Errorf("test err")),
+	)
+
+	testApp := fiber.New()
+	testApp.Put("/:ID", Update(mockStorage, testLog))
+	testApp.Put("/", Update(mockStorage, testLog))
+	defer utils.ExecuteWithLogError(testApp.Shutdown, testLog)
+
+	port := 3041
+	go func() {
+		err := testApp.Listen(":" + fmt.Sprintf("%d", port))
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	type args struct {
+		storage  storage.BaseStorage
+		log      logger.BaseLogger
+		paramURI string
+		body     []byte
+	}
+	type want struct {
+		statusCode int
+		body       []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "valid",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "",
+				body: []byte(`{
+									"Homework": {
+										"Id": 1,
+										"Name": "hw_name",
+										"Questions": [
+											{
+												"Id": 1,
+												"Name": "q1_name",
+												"Content": {
+													"Task": "task1",
+													"Answer": "answer1",
+													"Solution": "solution1"
+												}
+											}
+										]
+									}
+								}`),
+			},
+			want: want{
+				statusCode: fiber.StatusOK,
+				body:       []byte(`{"Homework":{"Id":1,"Name":"hw_name","Questions":[{"Id":1,"Name":"q1_name","Content":{"Id":0,"Task":"task1","Answer":"answer1","Solution":"solution1"}}]}}`),
+			},
+		},
+		{
+			name: "broken json",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "",
+				body: []byte(`{
+									Homework": {
+										"Id": 1,
+										"Name": "hw_name",
+										"Questions": [
+											{
+												"Id": 1,
+												"Name": "q1_name",
+												"Content": {
+													"Task": "task1",
+													"Answer": "answer1",
+													"Solution": "solution1"
+												}
+											}
+										]
+									}
+								}`),
+			},
+			want: want{
+				statusCode: fiber.StatusBadRequest,
+				body:       []byte(""),
+			},
+		},
+		{
+			name: "empty json body",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "",
+				body:     []byte(``),
+			},
+			want: want{
+				statusCode: fiber.StatusBadRequest,
+				body:       []byte(""),
+			},
+		},
+		{
+			name: "missing ID in URI and in body",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "/",
+				body: []byte(`{
+									"Homework": {
+										"Name": "hw_name",
+										"Questions": [
+											{
+												"Id": 1,
+												"Name": "q1_name",
+												"Content": {
+													"Task": "task1",
+													"Answer": "answer1",
+													"Solution": "solution1"
+												}
+											}
+										]
+									}
+								}`),
+			},
+			want: want{
+				statusCode: fiber.StatusBadRequest,
+				body:       []byte(""),
+			},
+		},
+		{
+			name: "ID in URI only",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "/1",
+				body: []byte(`{
+									"Homework": {
+										"Name": "hw_name",
+										"Questions": [
+											{
+												"Id": 1,
+												"Name": "q1_name",
+												"Content": {
+													"Task": "task1",
+													"Answer": "answer1",
+													"Solution": "solution1"
+												}
+											}
+										]
+									}
+								}`),
+			},
+			want: want{
+				statusCode: fiber.StatusOK,
+				body:       []byte(`{"Homework":{"Id":1,"Name":"hw_name","Questions":[{"Id":1,"Name":"q1_name","Content":{"Id":0,"Task":"task1","Answer":"answer1","Solution":"solution1"}}]}}`),
+			},
+		},
+		{
+			name: "ID in body only",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "/",
+				body: []byte(`{
+									"Homework": {
+										"Id": 1,
+										"Name": "hw_name",
+										"Questions": [
+											{
+												"Id": 1,
+												"Name": "q1_name",
+												"Content": {
+													"Task": "task1",
+													"Answer": "answer1",
+													"Solution": "solution1"
+												}
+											}
+										]
+									}
+								}`),
+			},
+			want: want{
+				statusCode: fiber.StatusOK,
+				body:       []byte(`{"Homework":{"Id":1,"Name":"hw_name","Questions":[{"Id":1,"Name":"q1_name","Content":{"Id":0,"Task":"task1","Answer":"answer1","Solution":"solution1"}}]}}`),
+			},
+		},
+		{
+			name: "empty homework name",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "",
+				body: []byte(`{
+									"Homework": {
+										"Id": 1,
+										"Name": "",
+										"Questions": [
+											{
+												"Id": 1,
+												"Name": "q1_name",
+												"Content": {
+													"Task": "task1",
+													"Answer": "answer1",
+													"Solution": "solution1"
+												}
+											}
+										]
+									}
+								}`),
+			},
+			want: want{
+				statusCode: fiber.StatusBadRequest,
+				body:       []byte(""),
+			},
+		},
+		{
+			name: "missing task in content",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "",
+				body: []byte(`{
+									"Homework": {
+										"Id": 1,
+										"Name": "hw1_name",
+										"Questions": [
+											{
+												"Id": 1,
+												"Name": "q1_name",
+												"Content": {
+													"Answer": "answer1",
+													"Solution": "solution1"
+												}
+											}
+										]
+									}
+								}`),
+			},
+			want: want{
+				statusCode: fiber.StatusBadRequest,
+				body:       []byte(""),
+			},
+		},
+		{
+			name: "missing question name",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "",
+				body: []byte(`{
+									"Homework": {
+										"Id": 1,
+										"Name": "hw1_name",
+										"Questions": [
+											{
+												"Id": 1,
+												"Name": "",
+												"Content": {
+													"Task": "task1",
+													"Answer": "answer1",
+													"Solution": "solution1"
+												}
+											}
+										]
+									}
+								}`),
+			},
+			want: want{
+				statusCode: fiber.StatusBadRequest,
+				body:       []byte(""),
+			},
+		},
+		{
+			name: "err from storage",
+			args: args{
+				storage:  nil,
+				log:      testLog,
+				paramURI: "",
+				body: []byte(`{
+									"Homework": {
+										"Id": 1,
+										"Name": "hw_name",
+										"Questions": [
+											{
+												"Id": 1,
+												"Name": "q1_name",
+												"Content": {
+													"Task": "task1",
+													"Answer": "answer1",
+													"Solution": "solution1"
+												}
+											}
+										]
+									}
+								}`),
+			},
+			want: want{
+				statusCode: fiber.StatusInternalServerError,
+				body:       []byte(""),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request, errReq := http.NewRequest(
+				http.MethodPut,
+				utilsReform.HostTest+fmt.Sprintf("%d", port)+tt.args.paramURI,
+				bytes.NewBuffer(tt.args.body),
+			)
+			require.NoError(t, errReq)
+
+			client := http.Client{}
+			response, errResp := client.Do(request)
+			require.NoError(t, errResp)
+			defer func() {
+				_ = response.Body.Close()
+			}()
+
+			assert.Equal(t, tt.want.statusCode, response.StatusCode)
+
+			respBody, err := io.ReadAll(response.Body)
+			require.NoError(t, err)
+			assert.Equal(t, string(tt.want.body), string(respBody))
+		})
+	}
+}
