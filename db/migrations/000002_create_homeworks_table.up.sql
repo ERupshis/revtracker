a07cc708d36b823f04f4b0e2834b236740fdfe77
Edgar Rupshis <73851167+ERupshis@users.noreply.github.com>
2023-11-08 14:22:31 +0200

    database. updated migrations. added triggers and functions for tables

--- db/migrations/000002_create_homeworks_table.up.sql
+++ db/migrations/000002_create_homeworks_table.up.sql
@@ -2,3 +2,21 @@ CREATE TABLE IF NOT EXISTS homeworks (
     id BIGSERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION remove_related_homework_questions()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF TG_OP = 'DELETE' THEN
+        DELETE FROM homework_questions WHERE id = OLD.homework_id;
+    END IF;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER remove_homework_questions_trigger
+    AFTER DELETE
+    ON homeworks
+    FOR EACH ROW
+EXECUTE FUNCTION remove_related_homework_questions();
--- db/migrations/000004_create_questions_table.down.sql
+++ db/migrations/000004_create_questions_table.down.sql
@@ -1,1 +1,11 @@
+DO
+$$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'questions') THEN
+            DROP TRIGGER IF EXISTS remove_contents_trigger ON questions;
+        END IF;
+    END
+$$;
+
 DROP TABLE IF EXISTS questions;
+DROP FUNCTION IF EXISTS remove_related_contents();
