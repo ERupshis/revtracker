1683ea1016dfe6269bafea8143aba364646221ef
Edgar Rupshis <73851167+ERupshis@users.noreply.github.com>
2023-11-08 13:52:34 +0200

    storage. reform. added trigger for questions table to remove related content

--- db/migrations/000004_create_questions_table.up.sql
+++ db/migrations/000004_create_questions_table.up.sql
@@ -1,5 +1,28 @@
-CREATE TABLE IF NOT EXISTS questions (
-    id   BIGSERIAL PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL,
-    content_id BIGINT REFERENCES contents(id) NOT NULL
+CREATE TABLE IF NOT EXISTS questions
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       TEXT UNIQUE                     NOT NULL,
+    content_id BIGINT REFERENCES contents (id) NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION remove_related_contents()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    IF TG_OP = 'DELETE' THEN
+        DELETE FROM contents WHERE id = OLD.content_id;
+    END IF;
+
+    IF TG_OP = 'UPDATE' AND OLD.content_id IS DISTINCT FROM NEW.content_id THEN
+        DELETE FROM contents WHERE id = OLD.content_id;
+    END IF;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER remove_contents_trigger
+    AFTER DELETE OR UPDATE
+    ON questions
+    FOR EACH ROW
+EXECUTE FUNCTION remove_related_contents();
